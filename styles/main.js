d9945a19e070e49b958e2cb26b4e21cdc35b748b
daihill84 <166342772+daihill84@users.noreply.github.com>
2025-03-23 01:31:42 +0000

    Add files via upload

---
+++ styles/main.js
@@ -0,0 +1,4 @@
+// Simple script to manage things like mobile responsiveness or button events if needed
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("Page Loaded Successfully!");
+});
